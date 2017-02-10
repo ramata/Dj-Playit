6d0dfa130cca7870e047ef3d81529233de356e53
Ramata Diallo <rama.diallo@gmail.com>
2017-02-09 23:29:59 -0500

    Modified production.rb file, to have background image showing in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,9 +21,9 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+  config.serve_static_files = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
